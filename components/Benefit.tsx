5d54933baf887c0ff4c2af6ba25efed01ce60377
Fernandoroyano <fernandoroyano.dev@gmail.com>
2025-11-30 15:59:05 +0100

    Actualizacion VITALIA

--- components/Benefit.tsx
+++ components/Benefit.tsx
@@ -4,7 +4,7 @@ export default function Benefit() {
       <div className="max-w-2xl w-full flex flex-col items-center gap-3">
         <span className="text-[rgb(0,94,184)] font-black text-5xl md:text-6xl">+200</span>
         <p className="text-[rgb(31,41,51)] font-bold text-xl md:text-2xl text-center">
-          familias han recuperado su autonomía física<br />gracias a <span className="text-[rgb(0,94,184)]">ANTEA Salud</span>
+          familias han recuperado su autonomía física<br />gracias a <span className="text-[rgb(0,94,184)]">VITALIA Salud</span>
         </p>
         <span className="block mt-2 text-[rgb(130,131,130)] text-base md:text-lg">Servicios en toda España</span>
       </div>
